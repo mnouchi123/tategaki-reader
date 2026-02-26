5c40e19f4bf5e5ad7cd4853f23c716bfda228cfe
mnouchi123 <68983238+mnouchi123@users.noreply.github.com>
2026-02-26 21:41:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tategaki-reader-v7';
+const CACHE_NAME = 'tategaki-reader-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
